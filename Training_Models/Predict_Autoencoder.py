68b4d7943d4e15a7c19e04de53dafe0435842f20
Niharika-Khare <30350859+Niharika-Khare@users.noreply.github.com>
2020-04-25 20:27:27 +0530

    Optimized Products_prediction()

--- Training_Models/Predict_Autoencoder.py
+++ Training_Models/Predict_Autoencoder.py
@@ -1,48 +1,62 @@
-from sklearn.metrics.pairwise import cosine_similarity
+import numpy as np
 import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
-def fetch_stored_pred(pred_df, user_id):
+def Fetch_stored_pred(Pred_df, UserID):
     temp = pd.DataFrame()
-    temp = pred_df[pred_df[0] == user_id]
+    temp = Pred_df[Pred_df[0] == UserID]
 
     all_prod = temp.iloc[0,1]
 
     return all_prod
 
-def Products_prediction(Cluster, user_id, Test_Vector, Col_List):
+def Products_prediction(Cluster, UserID, Test_Vector, Col_List):
 
-    cluster_num = Cluster[0]
-    all_cust = Cluster[1]
+    Cluster_Num = Cluster[0]
+    UserID_List = Cluster[1]
 
     # read the predictions for this cluster
-    pred_csv_path = './Autoencoder_Data/' + str(cluster_num) + "_preds.csv"
-    pred_df = pd.read_csv(pred_csv_path, header=None)
+    Pred_csv_path = './Autoencoder_Data/' + str(Cluster_Num) + "_preds.csv"
+    Pred_df = pd.read_csv(Pred_csv_path, header=None)
 
     temp = []
-    if user_id in all_cust:
+    if UserID in UserID_List:
         # existing user, fetch the prediction
-        temp = fetch_stored_pred(pred_df, user_id)
+        temp = Fetch_stored_pred(Pred_df, UserID)
     else:
         # load User_Product_Matrix corresponding to cluster
-        FilePath = './Clustering_Data/' + str(cluster_num) + 'UP_Mat.csv'
+        FilePath = './Clustering_Data/' + str(Cluster_Num) + 'UP_Mat.csv'
         User_Product_Matrix = pd.read_csv(FilePath, sep=',')
 
         # Determine User with highest cosine similarity wrt this user
-        max_sim = -1
-        for i in range(len(all_cust)):
-            sim = cosine_similarity([Test_Vector[Col_List]],[User_Product_Matrix.iloc[i][Col_List]])[0][0]
-            if sim>max_sim:
-                max_sim = sim
-                similar_user = int(User_Product_Matrix.iloc[i]['CustomerID'])
+        # MaxSimilarity = -1
 
-        temp = fetch_stored_pred(pred_df, similar_user)
+        # for i in range(len(UserID_List)):
+
+        #     Similarity = np.dot(Test_Vector[Col_List],User_Product_Matrix.iloc[i,1:])
+        #     # Similarity = cosine_similarity([Test_Vector[Col_List]],[User_Product_Matrix.iloc[i][Col_List]])[0][0]
+
+        #     if Similarity > MaxSimilarity:
+        #         MaxSimilarity = Similarity
+        #         Similar_user = int(User_Product_Matrix.iloc[i,0])
+
+        Data_Matrix = User_Product_Matrix.iloc[:,1:].transpose()
+        Test_Vector = np.array(Test_Vector[Col_List])
+        
+        Similarity = Test_Vector.dot(Data_Matrix)
+
+        Max_Similarity_index = np.where(Similarity == np.amax(Similarity))[0][0]
+
+        Similar_user = UserID_List[Max_Similarity_index]
+
+        temp = Fetch_stored_pred(Pred_df, Similar_user)
 
     temp = temp.replace("[", "")
     temp = temp.replace("]", "")
     temp = temp.split()
 
     results = list(map(int, temp))
-    print ("Reccommended product ids for user_" + str(user_id) + ": ")
-    # print (similar_user,':   ', results)
+    print ("Reccommended product ids for user_" + str(UserID) + ": ")
+    print (Similar_user,':   ', results)
 
     return results
